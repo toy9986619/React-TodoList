95359f12a644c9601409050a1aea58e98da854b4
toy9986619 <toy9986619@gmail.com>
2019-01-15 11:04:40 +0800

    完成 編輯資料內容功能

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,34 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const data = [
-    { id: 1, name: "testing01" }
+    { id: 0, name: "testing01" },
+    { id: 1, name: "testing02" }
 ];
 
-const ListUI = (props) => (
-    <ul>
-        {
-            props.data.map((item) => (
-                <li key={item.id}>{item.name}</li>
-            ))
-        }
-    </ul>
-);
+// const ListUI = (props) => (
+//     <ul>
+//         {
+//             props.data.map((item) => (
+//                 <li key={item.id}>{item.name}</li>
+//             ))
+//         }
+//     </ul>
+// );
 
 class TodoList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             data: data,
-            inputText: ''
+            inputText: '',
+            editTarget: null
         }
         this.onChange = this.onChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.changeEdit = this.changeEdit.bind(this);
+        this.editData = this.editData.bind(this);
+        this.saveEditData = this.saveEditData.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     onChange(e) {
         this.setState({
             inputText: e.target.value
         });
+    }
+
+    changeEdit(item) {
+        this.setState({ editTarget:item });
+    }
+
+    editData(e) {
+        this.setState({
+            editTarget: {
+                ...this.state.editTarget,
+                name: e.target.value
+            }
+        });
+    }
+
+    saveEditData(){
+        let index = this.state.data.findIndex(item => item.id == this.state.editTarget.id);
+        let data = [...this.state.data];
+        data[index] = this.state.editTarget;
+
+        this.setState({data: data});
     }
 
     handleSubmit(e) {
@@ -39,7 +66,17 @@ class TodoList extends React.Component {
         this.setState({ data: nextData, inputText: '' });
     }
 
+    handleKeyPress(e) {
+        console.log(e);
+        if(e.key == 'Enter'){
+            this.saveEditData()
+            this.setState({ editTarget: null })
+        }
+    }
+
     render() {
+        const editIndex = this.state.editTarget ? this.state.editTarget.id : null; 
+
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
@@ -48,7 +85,16 @@ class TodoList extends React.Component {
                     <button>加入</button>
                     <p>input: {this.state.inputText}</p>
                 </form>
-                <ListUI data={this.state.data} />
+                <ul>
+                    {
+                        this.state.data.map((item) => (
+                            editIndex == item.id ?
+                            <input type="text" onChange={this.editData} onKeyPress={this.handleKeyPress} 
+                                value={this.state.editTarget.name}></input>
+                            : <li onClick={() => {this.changeEdit(item)}} key={item.id}>{item.name}</li>
+                        ))
+                    }
+                </ul>
             </div>
         );
     }
