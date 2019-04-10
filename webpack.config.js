68eb3e58ca8b13b11a582501bc4cd6d0ed698b74
toy9986619 <toy9986619@gmail.com>
2019-04-10 13:46:24 +0800

    style: 排版修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,52 @@
 // HtmlWeboackPlugin config
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const HtmlWeboackPluginConfig = new HtmlWebpackPlugin({
-    template: `${__dirname}/src/index.html`,
-    filename: 'index.html',
-    inject: 'body'
-})
+  template: `${__dirname}/src/index.html`,
+  filename: 'index.html',
+  inject: 'body',
+});
 
 module.exports = {
-    entry: [
-        './src/index.js'
-    ],
+  entry: [
+    './src/index.js',
+  ],
 
-    output: {
-        path: `${__dirname}/dist`,
-        filename: 'app.js',
-    },
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'app.js',
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [
-                                "@babel/preset-env",
-                                "@babel/preset-react",
-                            ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react',
+              ],
 
-                            plugins: [
-                                "@babel/plugin-proposal-object-rest-spread",
-                                "@babel/plugin-proposal-class-properties"
-                            ]
-                        }
-                    }
-                ],
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/plugin-proposal-class-properties',
+              ],
             },
+          },
         ],
-    },
+      },
+    ],
+  },
 
-    // webpack-dev-server config
-    devServer: {
-        inline: true,
-        port: 8888,
-    },
+  // webpack-dev-server config
+  devServer: {
+    inline: true,
+    port: 8888,
+  },
 
-    plugins: [HtmlWeboackPluginConfig]
+  plugins: [HtmlWeboackPluginConfig],
 };
